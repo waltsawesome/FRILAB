e834e27ae9f0ad15c7b24a5ba5164a4b629b9023
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 10:20:12 -0500

    Don't worry bout' it

---
+++ sort.py
@@ -0,0 +1,13 @@
+ def bubble_sort(nums):
+
+     swapped = True
+     while swapped:
+         swapped = False
+         for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+
+Randomlist = [3,7,3,4,1,9] 
+bubble_sort(Ramdonlist)
+print(Randomlist)
